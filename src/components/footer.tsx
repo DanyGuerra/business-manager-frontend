9338a8c1050bd44704b700092691e2994e1f7898
DanyGuerra <luis_daniel.grra15@hotmail.com>
2025-09-03 22:22:26 -0600

    style: :lipstick: Footer page

---
+++ src/components/footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer(){
+    return(
+        <footer className="flex flex-col justify-center items-center p-3">
+            <h3>Business Manager</h3>
+            <p>© 2025 Business Manager. Todos los derechos reservados.</p>
+        </footer>
+    )
+}
