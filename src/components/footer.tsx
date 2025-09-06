555c1cc83e4bb5a7a2507a0775c40863ffe5ebe9
DanyGuerra <luis_daniel.grra15@hotmail.com>
2025-09-05 18:47:45 -0600

    feat: :sparkles: Footer and header

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,8 @@
-export default function Footer(){
-    return(
-        <footer className="flex flex-col justify-center items-center p-3">
-            <h3>Business Manager</h3>
-            <p>© 2025 Business Manager. Todos los derechos reservados.</p>
-        </footer>
-    )
+export default function Footer() {
+  return (
+    <footer className="flex flex-col justify-center items-center p-3">
+      <h3>Business Manager</h3>
+      <p>© 2025 Business Manager. Todos los derechos reservados.</p>
+    </footer>
+  );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,22 +2,23 @@ import Link from "next/link";
 import { ThemeToggle } from "./theme-toggle";
 import { Button } from "./ui/button";
 
-export default function Header(){
-    return (
-        <>
-        <header className="w-full flex justify-between p-1">
-             <Link href="/" className="flex justify-center items-center text-lg font-bold">
-                Business Manager   
-            </Link>
-            <div className="flex justify-center items-center gap-3">
-                <ThemeToggle></ThemeToggle>
-                 <Link href="/login">
-                    <Button className="cursor-pointer">
-                        Iniciar sesión
-                    </Button>
-                </Link>
-            </div>
-        </header>
-        </>
-    )
+export default function Header() {
+  return (
+    <>
+      <header className="w-full flex justify-between p-1">
+        <Link
+          href="/"
+          className="flex justify-center items-center text-lg font-bold"
+        >
+          Business Manager
+        </Link>
+        <div className="flex justify-center items-center gap-3">
+          <ThemeToggle></ThemeToggle>
+          <Link href="/login">
+            <Button className="cursor-pointer">Iniciar sesión</Button>
+          </Link>
+        </div>
+      </header>
+    </>
+  );
 }
