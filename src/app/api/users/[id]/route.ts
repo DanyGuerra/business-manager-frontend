640daebdb455c87fc424021b24baa65e574da984
DanyGuerra <luis_daniel.grra15@hotmail.com>
2025-09-10 21:23:52 -0600

    feat: :sparkles: User api

---
+++ src/app/api/users/[id]/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const res = await fetch(
+      `${process.env.API_BUSINESS_URL}/users/${params.id}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: req.headers.get("authorization") ?? "",
+        },
+        credentials: "include",
+      }
+    );
+
+    const data = await res.json();
+    return NextResponse.json(data, { status: res.status });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
