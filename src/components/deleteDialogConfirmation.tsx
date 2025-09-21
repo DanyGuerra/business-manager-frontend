3499b9c4a4fe11bbe4bc857167ed6db272dc18b0
DanyGuerra <luis_daniel.grra15@hotmail.com>
2025-09-21 17:51:16 -0600

    feat: :sparkles: Dialog delete confirmation

---
+++ src/components/deleteDialogConfirmation.tsx
@@ -0,0 +1,54 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import { Trash2Icon } from "lucide-react";
+
+type DeleteDialogConfirmationProps = {
+  title?: string;
+  description?: string;
+  handleCancel?: () => void;
+  handleContinue: () => void;
+};
+
+export function DeleteDialogConfirmation({
+  title = "¿Estás completamente seguro?",
+  description,
+  handleCancel = () => {},
+  handleContinue,
+}: DeleteDialogConfirmationProps) {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="outline" size="icon" className="cursor-pointer">
+          <Trash2Icon className="text-red-400"></Trash2Icon>
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{title}</AlertDialogTitle>
+          <AlertDialogDescription>{description}</AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel onClick={handleCancel} className="cursor-pointer">
+            Cancelar
+          </AlertDialogCancel>
+          <AlertDialogAction
+            onClick={handleContinue}
+            className="cursor-pointer"
+          >
+            Continuar
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
