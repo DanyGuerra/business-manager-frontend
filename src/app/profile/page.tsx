639e09c6df9a112384966089bf62d11741eab557
DanyGuerra <luis_daniel.grra15@hotmail.com>
2025-09-21 17:49:45 -0600

    feat: :sparkles: Profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,33 +1,39 @@
 "use client";
 
 import FormBusiness, { CreateBusinessValues } from "@/components/formBusiness";
-import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { PlusIcon } from "lucide-react";
-import { useBusinessApi } from "@/lib/useBusinessApi";
+import { Business, useBusinessApi } from "@/lib/useBusinessApi";
 import { toast } from "sonner";
 import { toastErrorStyle, toastSuccessStyle } from "@/lib/toastStyles";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AxiosError } from "axios";
+import BusinessCard from "@/components/businessCard";
+import { LoadingsKeyEnum, useLoadingStore } from "@/store/loadingStore";
+import CustomDialog from "@/components/formCustomDialog";
 
 export default function ProfilePage() {
-  const [open, setOpen] = useState(false);
+  const [businesses, setBusinesses] = useState<Business[]>([]);
   const businessApi = useBusinessApi();
+  const { startLoading, stopLoading } = useLoadingStore();
+  const [open, setOpen] = useState<boolean>(false);
+
+  async function getBusiness() {
+    try {
+      const { data } = await businessApi.getMyBusinesses();
+      setBusinesses(data);
+    } catch (error) {
+      toast.error("Algo salió mal al cargar los negocios");
+    }
+  }
 
   async function handleSubmit(dataCreate: CreateBusinessValues) {
     try {
+      startLoading(LoadingsKeyEnum.CREATE_BUSINESS);
       await businessApi.createBusiness(dataCreate);
+      await getBusiness();
       toast.success("Negocio creado con éxito", { style: toastSuccessStyle });
     } catch (error) {
       if (error instanceof AxiosError) {
-        toast.error(error.message, {
+        toast.error(error.response?.data.message, {
           style: toastErrorStyle,
         });
       } else {
@@ -36,32 +42,41 @@ export default function ProfilePage() {
         });
       }
     } finally {
+      stopLoading(LoadingsKeyEnum.CREATE_BUSINESS);
       setOpen(false);
     }
   }
 
+  useEffect(() => {
+    getBusiness();
+  }, []);
+
   return (
-    <section className="flex gap-4 items-center">
-      <div>Mis negocios</div>
-      <Dialog open={open} onOpenChange={setOpen}>
-        <DialogTrigger asChild>
-          <Button size="icon" className="size-8 cursor-pointer">
-            <PlusIcon />
-          </Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Crear negocio</DialogTitle>
-            <DialogDescription>
-              Crea un negocio y guardalo en tu perfil
-            </DialogDescription>
-          </DialogHeader>
+    <div className="flex flex-col gap-3">
+      <section className="flex gap-4 items-center">
+        <h1 className="text-2xl font-bold">Mis negocios</h1>
+        <CustomDialog
+          onOpenChange={setOpen}
+          modalTitle="Crear negocio"
+          modalDescription="Crea un negocio"
+        >
           <FormBusiness
             buttonTitle="Crear"
             handleSubmitButton={handleSubmit}
+            loadingKey={LoadingsKeyEnum.CREATE_BUSINESS}
           ></FormBusiness>
-        </DialogContent>
-      </Dialog>
-    </section>
+        </CustomDialog>
+      </section>
+      <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-5">
+        {businesses && businesses.length > 0
+          ? businesses.map((business) => (
+              <BusinessCard
+                key={business.id}
+                business={business}
+              ></BusinessCard>
+            ))
+          : "No hay negocios"}
+      </section>
+    </div>
   );
 }
