4861a086304e9de87d615efdbecb0f2a3166cbdb
DanyGuerra <luis_daniel.grra15@hotmail.com>
2025-09-21 17:43:19 -0600

    feat: :sparkles: Api product group

---
+++ src/app/api/product-group/[productGroupId]/route.ts
@@ -0,0 +1,58 @@
+import { NextRequest, NextResponse } from "next/server";
+import { NESTJS_URL, buildHeaders } from "../../headersUtils";
+
+export async function DELETE(
+  req: NextRequest,
+  context: { params: Promise<{ productGroupId: string }> }
+) {
+  try {
+    const { productGroupId } = await context.params;
+
+    const res = await fetch(`${NESTJS_URL}/product-group/${productGroupId}`, {
+      method: "DELETE",
+      headers: {
+        ...buildHeaders(req),
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    });
+
+    const data = await res.json();
+    return NextResponse.json(data, { status: res.status });
+  } catch (err) {
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function PATCH(
+  req: NextRequest,
+  context: { params: Promise<{ productGroupId: string }> }
+) {
+  try {
+    const { productGroupId } = await context.params;
+
+    const body = await req.json();
+
+    const res = await fetch(`${NESTJS_URL}/product-group/${productGroupId}`, {
+      method: "PATCH",
+      headers: {
+        ...buildHeaders(req),
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(body),
+    });
+
+    const data = await res.json();
+
+    return NextResponse.json(data, { status: res.status });
+  } catch (err) {
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
