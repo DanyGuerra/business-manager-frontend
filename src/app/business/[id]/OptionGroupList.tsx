277813d2f23106e17d3e4796d07dea425df04a33
DanyGuerra <luis_daniel.grra15@hotmail.com>
2025-09-26 18:43:56 -0600

    feat: :sparkles: Option group list

--- src/app/business/[id]/OptionGroupList.tsx
+++ src/app/business/[id]/OptionGroupList.tsx
@@ -2,6 +2,8 @@
 
 import { OptionGroup } from "@/lib/useBusinessApi";
 import OptionList from "./OptionList";
+import CustomDialog from "@/components/customDialog";
+import { ListPlusIcon } from "lucide-react";
 
 type OptionGroupListProps = {
   optionGroups: OptionGroup[];
@@ -12,7 +14,25 @@ export default function OptionGroupList({
 }: OptionGroupListProps) {
   return (
     <>
-      {optionGroups.length > 0 && (
+      <div className="flex items-center justify-center gap-3 mt-3">
+        <h1 className="text-sm font-bold">Variantes del producto</h1>
+        <div className="flex gap-1">
+          <CustomDialog
+            modalTitle="Agregar variante del producto"
+            modalDescription="Agrega un grupo de opciones existente para este producto"
+            icon={<ListPlusIcon />}
+          >
+            <></>
+          </CustomDialog>
+          <CustomDialog
+            modalTitle="Crear variante del producto"
+            modalDescription="Crea un nuevo grupo de opciones para este producto"
+          >
+            <></>
+          </CustomDialog>
+        </div>
+      </div>
+      {optionGroups.length > 0 ? (
         <div className="mt-2 space-y-1">
           {optionGroups.map((og) => (
             <div key={og.id}>
@@ -20,6 +40,10 @@ export default function OptionGroupList({
               <OptionList options={og.options} />
             </div>
           ))}
+        </div>
+      ) : (
+        <div className="flex items-center justify-center min-h-25">
+          <span className="text-muted-foreground">No hay variantes</span>
         </div>
       )}
     </>
