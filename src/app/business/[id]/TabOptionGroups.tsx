81aa051209389f9a1902fcf8b401cad34093db8e
DanyGuerra <luis_daniel.grra15@hotmail.com>
2025-11-19 22:00:33 -0600

    feat: display a message when no option groups are available

--- src/app/business/[id]/TabOptionGroups.tsx
+++ src/app/business/[id]/TabOptionGroups.tsx
@@ -84,71 +84,84 @@ export default function TabOptionGroups() {
   }, []);
 
   return (
-    <div className="grid py-4 grid-cols-1 gap-4 sm:grid-cols-2">
-      {optionGroups.map((og) => {
-        return (
-          <Card key={og.id}>
-            <CardHeader className="w-full">
-              <CardTitle className="flex items-center justify-between">
-                <span>{og.name}</span>
-                <span>
-                  <CustomDialog
-                    open={activeModal === og.id}
-                    setOpen={(state) => setActiveModal(state ? og.id : null)}
-                    modalTitle="Editar grupo de opciones"
-                    modalDescription={`Editar grupo de opcioones "${og.name}"`}
-                    icon={<Pencil />}
-                  >
-                    <FormProductOptionGroup
-                      buttonTitle="Guardar"
-                      loadingKey={LoadingsKeyEnum.UPDATE_GROUP_OPTION}
-                      defaultValues={{
-                        ...og,
-                        max_options: String(og.max_options),
-                        min_options: String(og.min_options),
-                      }}
-                      handleSubmitButton={(data) =>
-                        handleEditGroupOption(data, og.id)
-                      }
-                    />
-                  </CustomDialog>
-                  <DeleteDialogConfirmation
-                    handleContinue={() => deleteOptionGroup(og.id)}
-                  />
-                </span>
-              </CardTitle>
-              <CardDescription className="flex gap-2 items-center">
-                <Badge
-                  className={
-                    og.available
-                      ? "bg-green-100 text-green-700 border-green-300 text-xs"
-                      : "bg-red-100 text-red-700 border-red-300 text-xs"
-                  }
-                >
-                  {og.available ? "Disponible" : "No disponible"}
-                </Badge>
-                <div>Maximo: {og.max_options}</div>
-                <div>Minimo: {og.min_options}</div>
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="flex flex-col flex-wrap gap-2 w-full">
-              <div>
-                {og.options.length > 0 ? (
-                  og.options.map((o) => (
-                    <span className="rounded-md border p-2 text-sm" key={o.id}>
-                      {o.name} {o.price > 0 && `+ $${o.price}`}
+    <>
+      {optionGroups.length > 0 ? (
+        <div className="grid py-4 grid-cols-1 gap-4 sm:grid-cols-2">
+          {optionGroups.map((og) => {
+            return (
+              <Card key={og.id}>
+                <CardHeader className="w-full">
+                  <CardTitle className="flex items-center justify-between">
+                    <span>{og.name}</span>
+                    <span>
+                      <CustomDialog
+                        open={activeModal === og.id}
+                        setOpen={(state) =>
+                          setActiveModal(state ? og.id : null)
+                        }
+                        modalTitle="Editar grupo de opciones"
+                        modalDescription={`Editar grupo de opcioones "${og.name}"`}
+                        icon={<Pencil />}
+                      >
+                        <FormProductOptionGroup
+                          buttonTitle="Guardar"
+                          loadingKey={LoadingsKeyEnum.UPDATE_GROUP_OPTION}
+                          defaultValues={{
+                            ...og,
+                            max_options: String(og.max_options),
+                            min_options: String(og.min_options),
+                          }}
+                          handleSubmitButton={(data) =>
+                            handleEditGroupOption(data, og.id)
+                          }
+                        />
+                      </CustomDialog>
+                      <DeleteDialogConfirmation
+                        handleContinue={() => deleteOptionGroup(og.id)}
+                      />
                     </span>
-                  ))
-                ) : (
-                  <div className="p-2 flex justify-center items-center w-full text-muted-foreground">
-                    <div className="w-full">No hay opciones</div>
+                  </CardTitle>
+                  <CardDescription className="flex gap-2 items-center">
+                    <Badge
+                      className={
+                        og.available
+                          ? "bg-green-100 text-green-700 border-green-300 text-xs"
+                          : "bg-red-100 text-red-700 border-red-300 text-xs"
+                      }
+                    >
+                      {og.available ? "Disponible" : "No disponible"}
+                    </Badge>
+                    <div>Maximo: {og.max_options}</div>
+                    <div>Minimo: {og.min_options}</div>
+                  </CardDescription>
+                </CardHeader>
+                <CardContent className="flex flex-col flex-wrap gap-2 w-full">
+                  <div>
+                    {og.options.length > 0 ? (
+                      og.options.map((o) => (
+                        <span
+                          className="rounded-md border p-2 text-sm"
+                          key={o.id}
+                        >
+                          {o.name} {o.price > 0 && `+ $${o.price}`}
+                        </span>
+                      ))
+                    ) : (
+                      <div className="p-2 flex justify-center items-center w-full text-muted-foreground">
+                        <div className="w-full">No hay opciones</div>
+                      </div>
+                    )}
                   </div>
-                )}
-              </div>
-            </CardContent>
-          </Card>
-        );
-      })}
-    </div>
+                </CardContent>
+              </Card>
+            );
+          })}
+        </div>
+      ) : (
+        <div className="flex justify-center items-center h-48">
+          No hay grupo de opciones
+        </div>
+      )}
+    </>
   );
 }
