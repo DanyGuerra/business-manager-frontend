9fbe43100e9f5960187fc601bece3bc90b88a1ca
DanyGuerra <luis_daniel.grra15@hotmail.com>
2025-11-22 21:15:37 -0600

    feat: Refine header styling.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,17 +1,21 @@
 import Link from "next/link";
 import { ThemeToggle } from "./theme-toggle";
 import { Button } from "./ui/button";
+import { Store } from "lucide-react";
 
 export default function Header() {
   return (
     <>
-      <header className="w-full flex justify-between p-1">
-        <Link
-          href="/"
-          className="flex justify-center items-center text-lg font-bold"
-        >
-          Business Manager
-        </Link>
+      <header className="sticky top-0 z-50 w-full h-14 border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 flex justify-between p-1 items-center">
+        <div className="flex items-center gap-2">
+          <Store className="h-6 w-6" />
+          <Link
+            href="/"
+            className="flex justify-center items-center text-lg font-bold"
+          >
+            Business Manager
+          </Link>
+        </div>
         <div className="flex justify-center items-center gap-3">
           <ThemeToggle></ThemeToggle>
           <Link href="/login">
